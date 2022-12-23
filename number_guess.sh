3d3976f8776f7174b3170305cb98c81fdca633bc
edvardsmazprecnieks <edvards.mazprecnieks@code.berlin>
2022-12-23 22:27:55 +0000

    fix: fix psql commands

--- number_guess.sh
+++ number_guess.sh
@@ -4,15 +4,14 @@ PSQL="psql --username=freecodecamp --dbname=number_guessing_game -t --no-align -
 
 echo -e "\nEnter your username:"
 read USERNAME
-USER_ID=$($PSQL "SELECT user_id FROM users WHERE username = $USERNAME")
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username = '$USERNAME'")
 
 if [[ -z $USER_ID ]]
 then
-INSERT_USER_RESULT=$($PSL "INSERT INTO users(username) VALUES('$USERNAME')")
+INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES ('$USERNAME')")
 echo -e "\nWelcome, $USERNAME! It looks like this is your first time here"
 else
 GAMES_PLAYED=$($PSQL "SELECT COUNT(game_id) FROM games WHERE user_id = $USER_ID")
 BEST_GAME=$($PSQL "SELECT MAX(guesses) FROM games WHERE user_id = $USER_ID")
 echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
-
