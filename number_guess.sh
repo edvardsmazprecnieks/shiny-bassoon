8e6daf25c5e1f8fbad1916e05af7dbd4c156b699
edvardsmazprecnieks <edvards.mazprecnieks@code.berlin>
2022-12-23 23:41:12 +0000

    fix: different quick fixes

--- number_guess.sh
+++ number_guess.sh
@@ -3,34 +3,41 @@
 PSQL="psql --username=freecodecamp --dbname=number_guessing_game -t --no-align -c"
 
 echo -e "\nEnter your username:"
-read USERNAME
-USER_ID=$($PSQL "SELECT user_id FROM users WHERE username = '$USERNAME'")
+read USERNAME_INPUT
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username = '$USERNAME_INPUT'")
 
 if [[ -z $USER_ID ]]
 then
-INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES ('$USERNAME')")
-echo -e "\nWelcome, $USERNAME! It looks like this is your first time here"
+INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES ('$USERNAME_INPUT')")
+echo "Welcome, $USERNAME_INPUT! It looks like this is your first time here."
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username = '$USERNAME_INPUT'")
 else
+USERNAME=$($PSQL "SELECT username FROM users WHERE user_id = $USER_ID")
 GAMES_PLAYED=$($PSQL "SELECT COUNT(game_id) FROM games WHERE user_id = $USER_ID")
 BEST_GAME=$($PSQL "SELECT MAX(guesses) FROM games WHERE user_id = $USER_ID")
-echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+if [[ -z BEST_GAME ]]
+then
+BEST_GAME = 0
+fi
+echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
 
 SECRET_NUMBER=$((1+$RANDOM%1000))
-NUMBER_OF_GUESSES=1
 echo -e "\nGuess the secret number between 1 and 1000:"
 read PLAYER_GUESS
+NUMBER_OF_GUESSES=1
 
 while [ $SECRET_NUMBER != $PLAYER_GUESS ]
 do
 if [[ $PLAYER_GUESS =~ ^[0-9]+$ ]]
 then
-NUMBER_OF_GUESSES=$((NUMBER_OF_GUESSES+1))
 if (( $PLAYER_GUESS > $SECRET_NUMBER ))
 then
+NUMBER_OF_GUESSES=$((NUMBER_OF_GUESSES+1))
 echo -e "\nIt's lower than that, guess again:"
 read PLAYER_GUESS
 else
+NUMBER_OF_GUESSES=$((NUMBER_OF_GUESSES+1))
 echo -e "\nIt's higher than that, guess again:"
 read PLAYER_GUESS
 fi
