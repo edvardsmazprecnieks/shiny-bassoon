e27336c35e759746bc5df1874f9f336b78fdcb1e
edvardsmazprecnieks <edvards.mazprecnieks@code.berlin>
2022-12-23 22:43:09 +0000

    feat: Introduce game logic

--- number_guess.sh
+++ number_guess.sh
@@ -15,3 +15,20 @@ GAMES_PLAYED=$($PSQL "SELECT COUNT(game_id) FROM games WHERE user_id = $USER_ID"
 BEST_GAME=$($PSQL "SELECT MAX(guesses) FROM games WHERE user_id = $USER_ID")
 echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
+
+SECRET_NUMBER=$((1+$RANDOM%1000))
+echo $SECRET_NUMBER
+echo -e "\nGuess the secret number between 1 and 1000:"
+read PLAYER_GUESS
+
+while [ $SECRET_NUMBER != $PLAYER_GUESS ]
+do
+if (( $PLAYER_GUESS > $SECRET_NUMBER ))
+then
+echo -e "\nIt's lower than that, guess again:"
+read PLAYER_GUESS
+else
+echo -e "\nIt's higher than that, guess again:"
+read PLAYER_GUESS
+fi
+done
