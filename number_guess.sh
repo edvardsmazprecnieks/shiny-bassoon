cbdcf34524d179a8df04a022081f26dbae16b97d
edvardsmazprecnieks <edvards.mazprecnieks@code.berlin>
2022-12-23 22:05:16 +0000

    feat: find user_id from database

--- number_guess.sh
+++ number_guess.sh
@@ -2,3 +2,6 @@
 
 PSQL="psql --username=freecodecamp --dbname=number_guessing_game -t --no-align -c"
 
+echo -e "\nEnter your username:"
+read USERNAME
+USER_ID=$($PSQL "SELECT user_id FROM users WHERE username = $USERNAME")
